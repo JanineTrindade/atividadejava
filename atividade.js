f144f15d90b30790f01646049d5e2bf92434e7c2
JanineTrindade <janine.tquadros@outlook.com>
2021-10-18 21:43:11 -0300

    atualizar

---
+++ atividade.js
@@ -0,0 +1,90 @@
+//var media = 4;
+//var faltas = 1; 
+//var soma = 0;
+
+//if(media >=7) ///// média aluno ///////// 
+//{ 
+// console.log('Aprovado'); 
+//} //else if(media >=5 && faltas <2) 
+//{ // console.log('Recuperação'); 
+//} //else 
+//{ // console.log('Reprovado'); 
+// 
+//}
+
+// 
+//for(let numero = 1; numero <=10; numero ++) 
+///// contador até 100 
+/// 
+//{ 
+// console.log(numero); 
+//}
+ //
+
+//for(var i =1; i<=10; i++) 
+//{ 
+// soma +=i; 
+//} 
+//console.log(soma);
+
+//for(var i =8; i<=80; i+=8) ///tabuada do 8/// 
+//{ 
+// console.log(i); 
+//}
+
+//let x = 3; 
+//const y = x++;
+
+//console.log('x:${x},y:${y}');
+
+//for(var i =1; i<=10; i+=1) ///tabuada do 1 ao 10/// 
+//{ // console.log(i); //}
+
+for(var i =1; i<=10; i++) ///tabuada do 1/// 
+{ console.log("1*", + i + "=", 1*i); }
+
+for(var i =1; i<=10; i++) ///tabuada do 2/// 
+{ 
+    console.log( "2*", + i + "=", 2*i); 
+}
+
+for(var i =1; i<=10; i++) ///tabuada do 3///
+ 
+{
+      console.log("3*", + i + "=", 3*i); 
+} 
+for(var i =1; i<=10; i++) ///tabuada do 4///
+
+  
+{
+       console.log("4", + i + "=", 4*i); 
+    
+}
+     for(var i =1; i<=10; i++) ///tabuada do 5/// 
+{ 
+      console.log("5", + i + "=", 5*i); 
+}
+     for(var i =1; i<=10; i++) ///tabuada do 6///
+{
+        console.log("6", + i + "=", 6*i); 
+} 
+    for(var i =1; i<=10; i++) ///tabuada do 7/// 
+{ 
+       console.log("7", + i + "=", 7*i); 
+} 
+    for(var i =1; i<=10; i++) ///tabuada do 8/// 
+{ 
+       console.log("8",+ i + "=", 8*i); 
+} 
+    for(var i =1; i<=10; i++) ///tabuada do 9/// 
+{ 
+       console.log("9", + i + "=", 9*i); 
+} 
+    for(var i =1; i<=10; i++) ///tabuada do 10/// 
+{ 
+       console.log("10", + i + "=", 10*i);
+}
+
+//////////////// ATIVIDADE PRESENCIAL /////////////////
+
+/// Algoritmo////
